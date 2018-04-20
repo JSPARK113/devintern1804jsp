5d00ed6b86a839d427402dc6a201c481a4ae5961
JSPark <jisun.park113@gmail.com>
2018-04-20 15:24:45 +0900

    setup file

---
+++ README.md
@@ -0,0 +1,1 @@
+devintern1804jsp
--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name             = 'pyquibase',
+    name             = 'devintern1804jsp',
     version          = '1.0',
-    description      = 'Python wrapper for liquibase',
+    description      = 'devintern1804jsp',
     author           = 'JSP',
     author_email     = 'jisun.park113@gmail.com',
     url              = 'https://github.com/JSPARK113/devintern1804jsp',
     download_url     = '',
     install_requires = [ ],
     packages         = find_packages(),
-    keywords         = ['dataframe'],
-    python_requires  = '>=3'
+    keywords         = 'dataframe',
+    python_requires  = '>=3',
     zip_safe=False,
 
 )
