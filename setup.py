7a936e21285b2147d50096c88bb0ae5288e82190
JSPark <jisun.park113@gmail.com>
2018-04-20 15:05:27 +0900

    2nd commit

--- setup.py
+++ setup.py
@@ -1,1 +1,18 @@
 from setuptools import setup, find_packages
+
+
+setup(
+    name             = 'pyquibase',
+    version          = '1.0',
+    description      = 'Python wrapper for liquibase',
+    author           = 'JSP',
+    author_email     = 'jisun.park113@gmail.com',
+    url              = 'https://github.com/JSPARK113/devintern1804jsp',
+    download_url     = '',
+    install_requires = [ ],
+    packages         = find_packages(),
+    keywords         = ['dataframe'],
+    python_requires  = '>=3'
+    zip_safe=False,
+
+)
